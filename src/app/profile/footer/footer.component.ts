906a2040e593923c816f25fc983b65e769f0faf9
Chirag Parmar <chirag.parmar@sayalife.in>
2023-04-01 17:47:16 +0530

    init

---
+++ src/app/profile/footer/footer.component.ts
@@ -0,0 +1,16 @@
+import { DatePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+  public date:Date = new Date();
+  ngOnInit(): void {
+  }
+
+}
